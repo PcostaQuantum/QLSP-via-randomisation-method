a8e7e622540f942452a5608ec2e0856a243331dd
PcostaQuantum <85160765+PcostaQuantum@users.noreply.github.com>
2025-04-26 00:08:24 +1000

    Delete time_schedule_p.m

--- time_schedule_p.m
+++
@@ -1,9 +0,0 @@
-function [f] = time_schedule_p(p,t,kappa)
-%TIME_SCHEDULE Summary of this function goes here
-%   Detailed explanation goes here
-
-g = 1+t*(kappa^(p-1)-1);
-f = kappa/(kappa-1)*(1-g^(1/(-p+1)));
- 
-end
-
